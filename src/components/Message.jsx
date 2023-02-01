318a156194f78fbbf7377eb6008104eab44d5cbf
Aaron <aaron1venema@gmail.com>
2023-02-01 13:52:28 -0600

    Update Message.jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import {auth} from '../firebase'
 
 const style = {
-  message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
-  name: `absolute mt-[-4rem] text-gray-600 text-xs`,
-  sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
-  received: `bg-[#e5e5ea] text-black float-left rounded-br-full`,
+  message: `flex items-start shadow-xl m-4 py-2 px-3 rounded-tl-3xl rounded-tr-3xl`,
+  name: `absolute mt-[-1.75rem] text-gray-600 text-xs`,
+  sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-3xl`,
+  received: `bg-[#e5e5ea] text-black float-left rounded-br-3xl`,
 };
 
 const Message = ({ message }) => {
